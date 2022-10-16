5bf1c773b0204f690a3f02a1d920c3f4e9eba84e
AceF1ix <saadtanveer388@gmail.com>
2022-10-16 14:59:17 +0200

    Version Control

--- TombKeeper/Assets/Scripts/ParticleMarker.cs
+++ TombKeeper/Assets/Scripts/ParticleMarker.cs
@@ -19,6 +19,7 @@ public class ParticleMarker : MonoBehaviour
     private float radius = 10f;
     private int maxMarkers = 15;
     private Vector3 Neithrune_collected = new Vector3(-190.8f, 0.62f, 67.0f);
+    public string chaseSound = "Why?";
 
     // Start is called before the first frame update
     void Start()
@@ -77,6 +78,7 @@ public class ParticleMarker : MonoBehaviour
         GameObject.Find("Enemy").GetComponent<EnemyMovement>().New_Enemy_Pos(Neithrune_collected);
         neithruneCollect = true;
         runText.enabled = true;
+        FindObjectOfType<AudioManager>().Play(chaseSound);
     }
 
     public void PlaceRune()
